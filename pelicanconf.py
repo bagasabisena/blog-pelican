e44f737f5a07db9c8eff4a44f5d5bbeb0158ddd1
Bagas Abisena <bagasabisena@gmail.com>
2015-02-14 18:45:16 +0100

    add gallery using lightbox

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import datetime
+# import extended_sitemap
 
 AUTHOR = u'Bagas Abisena Swastanto'
 SITENAME = u'abisena.me'
@@ -8,13 +10,19 @@ SITEURL = ''
 
 PATH = 'content'
 
+# PLUGINS
+PLUGIN_PATHS = ['/Users/bagas/dev/github/pelican-plugins/']
+PLUGINS = ['extended_sitemap']
+
+# DATE TIME CONFIGURATION
 TIMEZONE = 'Europe/Amsterdam'
+CURRENT_DATE = datetime.now().isoformat()
 
 DEFAULT_LANG = u'en'
 
 # static files
-STATIC_PATHS = ['images', 'js', 'data', 'extra/CNAME']
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+STATIC_PATHS = ['images', 'js', 'data', 'lightbox', 'extra/CNAME', 'extra/robots.txt']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, 'extra/robots.txt': {'path': 'robots.txt'}}
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -32,7 +40,8 @@ AUTHOR_FEED_RSS = None
 # Social widget
 SOCIAL = (('Twitter', 'http://www.twitter.com/bagasabisena'),
           ('Flickr', 'http://www.flickr.com/bagasabisena'),
-          ('Github', 'http://www.github.com/bagasabisena'),)
+          ('Github', 'http://www.github.com/bagasabisena'),
+          ('Instagram', 'http://www.instagram.com/bagasabisena'),)
 
 DISPLAY_TAGS_INLINE = True
 
@@ -45,6 +54,7 @@ DISPLAY_ARTICLE_INFO_ON_INDEX = True
 # ABOUT ME
 ABOUT_ME = 'Master student Computer Science in TU Delft, NL.'
 AVATAR = 'images/bagas2.jpg'
+GRAVATAR_IMAGE = 'http://www.gravatar.com/avatar/f7b09b54c898a2db1bc7a20c74827bb6.png'
 
 DEFAULT_PAGINATION = 10
 
@@ -61,3 +71,25 @@ BANNER_SUBTITLE = 'A view from the afternoon'
 
 # FAVICON
 FAVICON = 'theme/images/kribo.ico'
+
+# THUMBNAILER SETTING
+IMAGE_PATH = 'gallery'
+THUMBNAIL_DIR = 'thumbnails'
+
+
+
+# SITEMAP
+# EXTENDED_SITEMAP_PLUGIN = {
+#     'priorities': {
+#         'index': 1.0,
+#         'articles': 0.8,
+#         'pages': 0.5,
+#         'others': 0.4
+#     },
+#     'changefrequencies': {
+#         'index': 'daily',
+#         'articles': 'weekly',
+#         'pages': 'monthly',
+#         'others': 'monthly',
+#     }
+# }
